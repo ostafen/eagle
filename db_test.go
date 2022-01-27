8f754e80760cff76fd9a3ed699fa4a5d5684343e
Stefano <stefano.scafiti96@gmail.com>
2022-01-27 21:06:32 +0100

    Refactor structure of tests

--- db_test.go
+++ db_test.go
@@ -8,11 +8,48 @@ import (
 	"math/rand"
 	"os"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
-func RandBytes(n int) []byte {
+type initFunc func(t *testing.T, db *DB) (interface{}, error)
+type checkFunc func(t *testing.T, db *DB, data interface{})
+
+func runEagleCheck(t *testing.T, opts *Options, init initFunc, check checkFunc) {
+	dir, err := ioutil.TempDir("", "eagle-test")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	opts.RootDir = dir
+
+	db, err := Open(*opts)
+	require.NoError(t, err)
+
+	data, err := init(t, db)
+	require.NoError(t, err)
+	check(t, db, data)
+
+	sizeBeforeClose := db.Size()
+
+	require.NoError(t, db.Close())
+
+	db, err = Open(*opts)
+	require.NoError(t, err)
+
+	sizeAfterOpen := db.Size()
+
+	if sizeAfterOpen != sizeBeforeClose {
+		t.Fatal("size mismatch")
+	}
+
+	check(t, db, data)
+	require.NoError(t, db.Close())
+}
+
+func genRandBytes(n int) []byte {
 	b := make([]byte, n)
 	rand.Read(b)
 	return b
@@ -26,73 +63,74 @@ func serialize(value uint32) []byte {
 
 const nRuns = 1000
 
-func insertMany(e *DB, n int, wg *sync.WaitGroup) {
+func putMany(e *DB, n int, wg *sync.WaitGroup, t *testing.T) {
+	var errors uint32
+
 	for i := 0; i < nRuns; i++ {
 		go func(i int) {
 			key := serialize(uint32(i))
 			value := serialize(uint32(i + 1))
 			err := e.Put(key, value)
 			if err != nil {
-				log.Fatal("put", err)
+				atomic.AddUint32(&errors, 1)
 			}
 			wg.Done()
 		}(i)
 	}
+
+	require.Equal(t, errors, uint32(0))
 }
 
-func removeEvenKeys(e *DB, n int, wg *sync.WaitGroup) {
+func removeEvenKeys(e *DB, n int, wg *sync.WaitGroup, t *testing.T) {
+	var errors uint32
+
 	for i := 0; i < nRuns; i += 2 {
 		go func(i int) {
 			key := serialize(uint32(i))
 
 			value, err := e.Get(key)
 			if err != nil {
-				log.Fatal(err)
+				atomic.AddUint32(&errors, 1)
 			}
 
 			for value == nil { // this is necessary, because the key may have not been inserted at the time we execute remove
 				value, err = e.Get(key)
 				if err != nil {
-					log.Fatal(err)
+					atomic.AddUint32(&errors, 1)
 				}
 				time.Sleep(time.Millisecond * 500)
-
 			}
 
 			err = e.Remove(key)
 			if err != nil {
-				log.Fatal(err)
+				atomic.AddUint32(&errors, 1)
 			}
 
 			wg.Done()
 		}(i)
 	}
+
+	require.Equal(t, errors, uint32(0))
 }
 
-func TestWritesAndReload(t *testing.T) {
-	dbDir := "./db"
-	defer os.RemoveAll(dbDir)
+func TestConcurrentPut(t *testing.T) {
+	key := genRandBytes(16)
+	opts := DefaultOptions("").WithEncryptionKey(key)
 
-	key := RandBytes(16)
-
-	opts := DefaultOptions(dbDir).WithEncryptionKey(key)
-	e, err := Open(opts)
-	if err != nil {
-		t.Fatal("open", err)
+	init := func(t *testing.T, db *DB) (interface{}, error) {
+		wg := &sync.WaitGroup{}
+		wg.Add(nRuns)
+		putMany(db, nRuns, wg, t)
+		wg.Wait()
+		return nil, nil
 	}
-	rand.Seed(time.Now().UnixNano())
 
-	wg := &sync.WaitGroup{}
-	wg.Add(nRuns)
-	insertMany(e, nRuns, wg)
-	wg.Wait()
-
-	runCheck := func(db *DB) {
+	runCheck := func(t *testing.T, db *DB, data interface{}) {
 		for i := 0; i < nRuns; i++ {
 			key := serialize(uint32(i))
-			value, err := e.Get(key)
+			value, err := db.Get(key)
 			if err != nil {
-				log.Fatal("get", err)
+				t.Fatal("get", err)
 			}
 
 			deser := binary.BigEndian.Uint32(value)
@@ -101,62 +139,31 @@ func TestWritesAndReload(t *testing.T) {
 			}
 		}
 	}
-	runCheck(e)
-
-	err = e.Close()
-	if err != nil {
-		log.Fatal("close", err)
-	}
-
-	log.Println("after opening")
-
-	e, err = Open(opts)
-	if err != nil {
-		t.Fatal("open", err)
-	}
-
-	runCheck(e)
-
-	err = e.Close()
-	if err != nil {
-		log.Fatal("close", err)
-	}
+	runEagleCheck(t, &opts, init, runCheck)
 }
 
-func TestInsertRemove(t *testing.T) {
-	dbDir, err := ioutil.TempDir(".", "db")
-	if err != nil {
-		t.Fatal(err)
+func TestConcurrentPutAndRemove(t *testing.T) {
+	key := genRandBytes(16)
+	opts := DefaultOptions("").WithEncryptionKey(key)
+
+	init := func(t *testing.T, db *DB) (interface{}, error) {
+		wg := &sync.WaitGroup{}
+		wg.Add(nRuns + (nRuns / 2))
+
+		putMany(db, nRuns, wg, t)
+
+		removeEvenKeys(db, nRuns, wg, t)
+		wg.Wait()
+		return nil, nil
 	}
-	defer os.RemoveAll(dbDir)
 
-	key := RandBytes(16)
-
-	opts := DefaultOptions(dbDir).WithEncryptionKey(key)
-
-	e, err := Open(opts)
-	if err != nil {
-		t.Fatal("get", err)
-	}
-	rand.Seed(time.Now().UnixNano())
-
-	wg := &sync.WaitGroup{}
-	wg.Add(nRuns + (nRuns / 2))
-
-	insertMany(e, nRuns, wg)
-
-	time.Sleep(time.Second * 6)
-	removeEvenKeys(e, nRuns, wg)
-
-	wg.Wait()
-
-	runCheck := func(e *DB) {
+	runCheck := func(t *testing.T, db *DB, data interface{}) {
 		for i := 0; i < nRuns; i++ {
 			key := serialize(uint32(i))
 
-			value, err := e.Get(key)
+			value, err := db.Get(key)
 			if err != nil {
-				log.Fatal("get", err)
+				t.Fatal("get", err)
 			}
 
 			even := i%2 == 0
@@ -166,7 +173,7 @@ func TestInsertRemove(t *testing.T) {
 
 			if !even {
 				if value == nil {
-					log.Fatal("nil value found", i)
+					t.Fatal("nil value found", i)
 				}
 
 				deser := binary.BigEndian.Uint32(value)
@@ -176,134 +183,70 @@ func TestInsertRemove(t *testing.T) {
 			}
 		}
 	}
-
-	runCheck(e)
-	log.Println(e.Size())
-
-	err = e.Close()
-	if err != nil {
-		t.Fatal("get", err)
-	}
-
-	e, err = Open(opts)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	runCheck(e)
-
-	log.Println(e.Size())
-
-	err = e.Close()
-	if err != nil {
-		t.Fatal("get", err)
-	}
+	runEagleCheck(t, &opts, init, runCheck)
 }
 
 type Bytes []byte
 
 func TestUpdateSameKeys(t *testing.T) {
-	dbDir, err := ioutil.TempDir(".", "db")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(dbDir)
-
-	key := RandBytes(16)
+	key := genRandBytes(16)
 
 	// ensure to trigger file compaction very often
-	opts := DefaultOptions(dbDir).WithEncryptionKey(key).WithMaxFileSize(4096).WithFileCompactionThreshold(0.1)
+	opts := DefaultOptions("").WithEncryptionKey(key).WithMaxFileSize(4096).WithFileCompactionThreshold(0.1)
 
-	db, err := Open(opts)
-	if err != nil {
-		t.Fatal("get", err)
-	}
 	rand.Seed(time.Now().UnixNano())
 
-	keySpace := 100
-	keySize := 10
-	valueSize := 25
+	init := func(t *testing.T, db *DB) (interface{}, error) {
+		keySpace := 100
+		keySize := 10
+		valueSize := 25
 
-	keys := make([]Bytes, keySpace)
-	for i := 0; i < keySpace; i++ {
-		keys[i] = RandBytes(keySize)
-	}
-
-	oracleMap := make(map[string]Bytes)
-	for i := 0; i < nRuns; i++ {
-		key := keys[rand.Intn(keySpace)]
-
-		if !bytes.Equal([]byte(string(key)), key) {
-			t.Fatal("conversion problem")
+		keys := make([]Bytes, keySpace)
+		for i := 0; i < keySpace; i++ {
+			keys[i] = genRandBytes(keySize)
 		}
 
-		var value []byte = nil
-		if rand.Int()%2 == 0 {
-			value = RandBytes(valueSize)
-			oracleMap[string(key)] = value
+		oracleMap := make(map[string]Bytes)
+		for i := 0; i < nRuns; i++ {
+			key := keys[rand.Intn(keySpace)]
 
-			if err := db.Put(key, value); err != nil {
+			if !bytes.Equal([]byte(string(key)), key) {
+				t.Fatal("conversion problem")
+			}
+
+			var value []byte = nil
+			if rand.Int()%2 == 0 {
+				value = genRandBytes(valueSize)
+				oracleMap[string(key)] = value
+
+				if err := db.Put(key, value); err != nil {
+					t.Fatal(err)
+				}
+			} else {
+				oracleMap[string(key)] = nil
+
+				if err := db.Remove(key); err != nil {
+					t.Fatal(err)
+				}
+			}
+		}
+		return oracleMap, nil
+	}
+
+	runCheck := func(t *testing.T, db *DB, data interface{}) {
+		oracleMap := data.(map[string]Bytes)
+
+		for key, value := range oracleMap {
+			retrievedValue, err := db.Get([]byte(key))
+			if err != nil {
 				t.Fatal(err)
 			}
-		} else {
-			oracleMap[string(key)] = nil // remember which keys have been deleted
 
-			if err := db.Remove(key); err != nil {
-				t.Fatal(err)
+			if !bytes.Equal(retrievedValue, value) {
+				log.Println(retrievedValue, value)
+				t.Fatal("value mismatch")
 			}
 		}
-
-		retrievedValue, err := db.Get(key)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if !bytes.Equal(retrievedValue, value) {
-			t.Fatal("value mismatch")
-		}
 	}
-
-	sizeBeforeClose := db.Size()
-	mapSize := 0
-	for _, value := range oracleMap {
-		if value != nil {
-			mapSize++
-		}
-	}
-
-	if sizeBeforeClose != mapSize {
-		t.Fatalf("db size and map size mismatch: expected %d, found %d\n", sizeBeforeClose, mapSize)
-	}
-
-	if err := db.Close(); err != nil {
-		t.Fatal(err)
-	}
-	db, err = Open(opts)
-	if err != nil {
-		t.Fatal("open", err)
-	}
-
-	if db.Size() != sizeBeforeClose {
-		t.Fatalf("db size after close is different: expected %d, found %d\n", sizeBeforeClose, db.Size())
-	}
-	defer db.Close()
-
-	deleted := 0
-	for _, value := range oracleMap {
-		if value == nil {
-			deleted++
-		}
-	}
-
-	for key, value := range oracleMap {
-		retrievedValue, err := db.Get([]byte(key))
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if !bytes.Equal(retrievedValue, value) {
-			log.Println(retrievedValue, value)
-			t.Fatal("value mismatch")
-		}
-	}
+	runEagleCheck(t, &opts, init, runCheck)
 }
